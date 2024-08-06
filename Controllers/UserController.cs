f6583ada416526783728123ec2d643f5eb3db90a
21IT008 <21it008@charusat.edu.in>
2024-08-07 00:54:30 +0530

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OmdotnetMvc.Models;
+using System.Data.SqlClient;
+
+namespace OmdotnetMvc.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        public UserController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                string connectionString = _configuration.GetConnectionString("DefaultConnection");
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    if(user.AcceptTerms==false)
+                    {
+                        ModelState.AddModelError("AcceptTerms", "Please accept terms and conditions");
+                        return View(user);
+                    }
+                    using (SqlCommand Checkcommand = new SqlCommand("SELECT COUNT(*) FROM Users WHERE Email=@Email", connection))
+                    {
+                        Checkcommand.Parameters.AddWithValue("@Email", user.Email);
+                        int count = (int)Checkcommand.ExecuteScalar();
+                        if (count > 0)
+                        {
+                            ModelState.AddModelError("Email", "Email already exists");
+                            return View(user);
+                        }
+                    }
+                    using(SqlCommand InsertCommand =new SqlCommand("INSERT INTO USERS (Name,Email,Password,Gender,AcceptTerms,Country) VALUES (@Name,@Email,@Password,@Gender,@AcceptTerms,@Country)",connection))
+                    {
+                        InsertCommand.Parameters.AddWithValue("@Name", user.Name);
+                        InsertCommand.Parameters.AddWithValue("@Email", user.Email);
+                        InsertCommand.Parameters.AddWithValue("@Password", user.Password);
+                        InsertCommand.Parameters.AddWithValue("@Gender",user.Gender);
+                        InsertCommand.Parameters.AddWithValue("@AcceptTerms",user.AcceptTerms);
+                        InsertCommand.Parameters.AddWithValue("@Country",user.Country);
+                        InsertCommand.ExecuteNonQuery();
+                    }
+                }
+                return RedirectToAction("Success");
+            }
+            return View(user);
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OmdotnetMvc.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage ="Plaese Provide Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="Plaese Provide Email")]
+        [EmailAddress(ErrorMessage ="Invalid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Plaese Confirm Email")]
+        [Compare("Email",ErrorMessage ="Emails are not matching")]
+        public string ConfirmEmail { get; set; }
+
+        [Required(ErrorMessage = "Plaese Enter Password")]
+        [StringLength(10,MinimumLength =6,ErrorMessage ="password should be between 6 t0 10 characters")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Plaese Choose Gender")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Plaese Accpet Terms")]
+        public bool AcceptTerms { get; set; }
+
+        [Required(ErrorMessage = "Plaese Choose Country")]
+        public string Country { get; set; }
+    }
+}
