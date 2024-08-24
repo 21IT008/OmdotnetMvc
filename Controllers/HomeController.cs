6c436d55ced0eaa7194c0f0ba00dc64e10c43e6a
21IT008 <21it008@charusat.edu.in>
2024-08-24 17:15:18 +0530

    revised home page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using OmdotnetMvc.Models;
+using System.Data.SqlClient;
 using System.Diagnostics;
 
 namespace OmdotnetMvc.Controllers
@@ -7,14 +8,27 @@ namespace OmdotnetMvc.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
         {
+            int totalrecords = 0;
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand("SELECT COUNT(*) FROM Users", connection))
+                {
+                    totalrecords = (int)command.ExecuteScalar();
+                }
+            }
+            ViewBag.TotalRecords = totalrecords;
             return View();
         }
 
